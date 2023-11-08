bf4829d1200ec42a0c5bddb67364384d68f6b33f
Thomas CORDONNIER <T_cordonnier@yahoo.fr>
2023-11-08 11:26:04 +0100

    Simplification return

--- project_saved/set-latest-translations.groovy
+++ project_saved/set-latest-translations.groovy
@@ -1,8 +1,8 @@
 /* :name = Set Latest Translations :description=
  *
- * @author	Manuel Souto Pico
- * @creation	2023.11.06
- * @version	0.0.1
+ * @author	Thomas CORDONNIER
+ * @creation	2023.11.07
+ * @version	0.1.1
 */
 
 
@@ -25,26 +25,29 @@ def gui() {
 		if (name.startsWith(path_to_tmx_dir)) {
 			console.println("Importing from " + name);
 			tmx.entries.each { entry ->
-				// Search which entry in the project corresponds to the one in the tmx
+				// Search which entry in the source files corresponds to the one in the tmx
 				// Note: to be improved, for the moment it works only with default entries
 				// and it is not optimized, we should use a cache as in ImportFromAutoTMX
-				def inProject = null
+				def inSourceFilesEntry = null
 				project.allEntries.each { pe -> 
-					if (pe.srcText.equals(entry.source)) inProject = pe;
+					if (pe.srcText.equals(entry.source)) inSourceFilesEntry = pe;
 				}
 				// Now search is done, if we found something we use it
-				if ((inProject != null) && (entry.source.equals(inProject.srcText))) {
-					def inProjectEntry = project.getTranslationInfo(inProject)
-					if ((inProjectEntry != null) && (entry.source.equals(inProjectEntry.source))) {
-						long inProjectDate = inProjectEntry.creationDate
-						if (inProjectEntry.changeDate > inProjectEntry.creationDate) inProjectDate = inProjectEntry.changeDate
-						long inTmxDate = entry.creationDate
-						if (entry.changeDate > entry.creationDate) inTmxDate = entry.changeDate
-						console.println(entry.source + " " + inTmxDate + " / " + inProjectDate + " => " + (inTmxDate > inProjectDate));
-						if (inTmxDate > inProjectDate) {
-							project.setTranslation(inProject, entry, true, null);// org.omegat.core.data.TMXEntry.ExternalLinked.xAUTO);
-						}
-					}
+				if (inSourceFilesEntry == null) return; // not found => next tmx.entry
+				if (! entry.source.equals(inSourceFilesEntry.srcText)) throw new Exception("Something got wrong: " + entry.source + " found " + inSourceFilesEntry.source);
+				// Still here? We have found an entry in source files, now search in project_save.tmx
+				def inProjectSaveEntry = project.getTranslationInfo(inSourceFilesEntry)
+				if (inProjectSaveEntry == null) return;	// entry is in source but not translated
+				if (! entry.source.equals(inProjectSaveEntry.source)) return;	// same but OmegaT returns a dummy entry
+				if (entry.translation.equals(inProjectSaveEntry.translation)) return;	// in this case you don't want to update
+				// Still here? Now let's check the date
+				long inProjectDate = inProjectSaveEntry.creationDate
+				if (inProjectSaveEntry.changeDate > inProjectSaveEntry.creationDate) inProjectDate = inProjectSaveEntry.changeDate
+				long inTmxDate = entry.creationDate
+				if (entry.changeDate > entry.creationDate) inTmxDate = entry.changeDate
+				console.println(entry.source + " " + inTmxDate + " / " + inProjectDate + " => " + (inTmxDate > inProjectDate));
+				if (inTmxDate > inProjectDate) {
+					project.setTranslation(inSourceFilesEntry, entry, true, org.omegat.core.data.TMXEntry.ExternalLinked.xAUTO);
 				}
 			}
 		}
