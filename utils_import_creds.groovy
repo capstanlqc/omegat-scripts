08c9c3d25e2aa88344ac8003ae977fe480d2d650
Kos Ivantsov <kos.ivantsov@gmail.com>
2024-08-24 15:55:41 +0200

    Update utils_import_creds.groovy
    overwrite lines if the imported file contains creds for repos already existing in 'repositories.properties'

--- utils_import_creds.groovy
+++ utils_import_creds.groovy
@@ -2,7 +2,8 @@
  *
  * @author      Manuel Souto Pico, Kos Ivantsov
  * @date        2024-06-20
- * @version     0.0.3
+ * @update      2024-08-24 [overwrite lines if the imported file contains creds for repos already existing in 'repositories.properties']
+ * @version     0.1
  */
 
 import java.text.SimpleDateFormat
@@ -12,15 +13,15 @@ import javax.swing.JFileChooser
 import org.omegat.util.StaticUtils
 import static javax.swing.JOptionPane.*
 
-// define constants
+// Define constants
 configDir = StaticUtils.getConfigDir()
 credsFile = new File(configDir + File.separator + "repositories.properties")
 
-// titles and info in console
+// Titles and info in console
 scriptName = "Import Credentials"
 scriptConsoleTag = "${scriptName}\n${"="*scriptName.size()}"
 
-// function to check for binary files
+// Function to check for binary files
 def isBinaryFile(File file) {
     buffer = new byte[1024]
     int bytesRead = file.withInputStream { it.read(buffer) }
@@ -35,13 +36,22 @@ def isBinaryFile(File file) {
     return false
 }
 
-// function to check if file contains "!username=" and "!password="
+// Function to check if file contains "!username=" and "!password="
 def containsRequiredText(File file) {
     text = file.text
-    return text.contains('!username=') && text.contains('!password=')
+    return text.contains("!username=") && text.contains("!password=")
 }
 
-// select the creds file to import
+// Function to strip username and password from lines
+// It will be used when comparing lines in the existing and the imported files
+def normalizeLine(line) {
+    line = line
+        .replaceAll(/!password=.*/, /!password=/)
+        .replaceAll(/!username=.*/, /!username=/)
+    return line
+}
+
+// Select a creds file to import
 console.println("${scriptConsoleTag}")
 fileChooser = new JFileChooser()
 fileChooser.dialogTitle = scriptName
@@ -52,27 +62,27 @@ boolean validFileSelected = false
 while (!validFileSelected) {
     int result = fileChooser.showOpenDialog()
     if (result == JFileChooser.APPROVE_OPTION) {
-        // get the selected file
+        // Get the selected file
         selectedFile = fileChooser.getSelectedFile()
         console.println("Selected file: ${selectedFile.getAbsolutePath()}")
-        // check the file extension
+        // Check the file extension
         if (selectedFile.name.endsWith(".done")) {
-            // show an error dialog if the file has ".done" extension
+            // Show an error dialog if the file has ".done" extension
             message = "The selected file has already been processed.\nSelect another file."
             showMessageDialog(null, message, "Invalid File", ERROR_MESSAGE)
             console.println(message)
         } else if (isBinaryFile(selectedFile)) {
-            // show an error dialog if the file is a binary
+            // Show an error dialog if the file is a binary
             message = "The selected file is a binary file and cannot be used.\nSelect another file."
             showMessageDialog(null, message, "Invalid File", ERROR_MESSAGE)
             console.println(message)
         } else if (!containsRequiredText(selectedFile)) {
-            // show an error dialog if the file does not contain '!username=' and '!password='
+            // Show an error dialog if the file does not contain '!username=' and '!password='
             message = "The selected file does not contain credentials.\nSelect another file."
             showMessageDialog(null, message, "Invalid File", ERROR_MESSAGE)
             console.println(message)
         } else {
-            // valid file is selected
+            // Valid file is selected
             console.println("The selected file is being imported.")
             validFileSelected = true
         }
@@ -83,7 +93,7 @@ while (!validFileSelected) {
     }
 }
 
-// create timestamps
+// Create timestamps
 date = new Date()
 bakFormat = new SimpleDateFormat("yyyyMMdd.HHmmss")
 bakFormattedDate = bakFormat.format(date)
@@ -91,10 +101,10 @@ headerFormat = new SimpleDateFormat("'#'EEE MMM dd HH:mm:ss z yyyy")
 headerFormat.setTimeZone(TimeZone.getTimeZone("CET"))
 formattedDate = headerFormat.format(date)
 
-// create or backup creds file
+// Create or backup creds file
 if (credsFile.exists()) {
 	// make backup
-	backupFilePath = credsFile.getAbsolutePath() + '.' + bakFormattedDate + '.bak'
+	backupFilePath = credsFile.getAbsolutePath() + "." + bakFormattedDate + ".bak"
 	backupFile = new File(backupFilePath)
 	backupFile.text = credsFile.text
 	console.println("Backing up the creds file as ${backupFilePath}.")
@@ -102,19 +112,39 @@ if (credsFile.exists()) {
 	console.println("Creating the creds file in OmegaT config dir.")
 	credsFile.createNewFile() // empty
 	credsFile.text = formattedDate + "\n"
-	// credsFile.write(formattedDate)
+	//credsFile.write(formattedDate)
 }
 
-// combine OmegaT creds file and the selected file, dedupe and write
+// Collect lines from the credentials file and the selected file into arrays
+credsFileLines = credsFile.text.tokenize("\n")
+selectedFileLines = selectedFile.text.tokenize("\n")
+
+// Remove comments in the selected file
+selectedFileLines.removeIf { line ->
+    line.trim().startsWith("#")
+}
+
+// Collect URLs in the selected file; lines with these URLs will be deleted from the credentials array before merging
+linesToRemove = selectedFileLines.collect { normalizeLine(it) }
+// Remove collected URLs in the existing credentials array
+credsFileLines = credsFileLines.findAll { line ->
+    normalizedLine = normalizeLine(line)
+    !linesToRemove.contains(normalizedLine)
+}
+
+// Combine contents of OmegaT creds file and the selected file, dedupe and write
 credsContents = new StringWriter()
-credsContents << credsFile.text
-credsContents << "\n${selectedFile.text}"
+credsContents << credsFileLines.join("\n")
+credsContents << "\n${selectedFileLines.join("\n")}"
+// Convert to an array again to dedupe
 finalContents = credsContents.toString().tokenize("\n")
+// Dedupe and write
 credsFile.text = finalContents.unique().join("\n").toString()
 
-// mark selected file as done
-doneFilePath = selectedFile.getAbsolutePath() + '.done'
+// Mark selected file as done
+doneFilePath = selectedFile.getAbsolutePath() + ".done"
 doneFile = new File(doneFilePath)
 selectedFile.renameTo(doneFile)
+console.println("Credentials imported from $selectedFile")
 
 return
