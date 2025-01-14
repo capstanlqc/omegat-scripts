5bca64ba7c3343b509412e02e66cba0d69b0c7ac
Kos Ivantsov <kos.ivantsov@gmail.com>
2025-01-14 18:20:01 +0100

    Update checkOMTversion.groovy
    close the project only after the user has been informed about the outdated version

--- project_changed/checkOMTversion.groovy
+++ project_changed/checkOMTversion.groovy
@@ -2,10 +2,11 @@
  * 
  * @author:  Kos Ivantsov
  * @date:    2024-02-12
- * @version: 0.2.2
+ * @version: 0.2.3
  * @changed: Manuel 2024-02-20 -- fixed matching regex, changed order of actions (close, then prompt)
  * @changed: Manuel 2024-04.24 -- remove open project / project name and event type checks to run in any case
  * @changed: Manuel 2024-06-27 -- add a list of allowed revisions (rather than unique version)
+ * @changed: Kos    2025-01014 -- close the project only after the user has been informed
  */
 
 import java.awt.Desktop
@@ -16,9 +17,9 @@ import static org.omegat.core.events.IProjectEventListener.PROJECT_CHANGE_TYPE.*
 import static org.omegat.gui.main.ProjectUICommands.*
 import static org.omegat.util.Platform.*
 
-reqVersion = "5.7.2"
+reqVersion = "5.7.3"
 // reqRevision = "a978d82ee"
-allowedRevisions = ["a978d82ee", "4e7e1433e", "4c233b15c"]
+allowedRevisions = ["a978d82ee", "57b1bb571", "e363cb094"] // 57b1bb571 was used for Windows, e363cb094 was used for Mac
 winURL="https://cat.capstan.be/OmegaT/exe/OmegaT_${reqVersion}_Windows_64_Signed.exe"
 macURL="https://cat.capstan.be/OmegaT/exe/OmegaT_${reqVersion}_Mac.zip"
 
@@ -30,14 +31,16 @@ if (eventType == LOAD) {
     if (!(allowedRevisions.contains(OStrings.REVISION))) {
         
         // close the project, first of all
-        org.omegat.util.gui.UIThreadsUtil.executeInSwingThread { projectClose() }
+        //org.omegat.util.gui.UIThreadsUtil.executeInSwingThread { projectClose() }
 
         // inform the user 
-        msg="OmegaT 5.7.2 (built by cApStAn) is required.  "
+        msg="OmegaT ${reqVersion} (built by cApStAn) is required."
         console.println("== ${title} ==")
         console.println(msg)
         showMessageDialog null, msg, title, INFORMATION_MESSAGE
         openURL = true
+        // close the project
+        projectClose()
     } else {
         console.println("== ${title} ==")
         console.println("OmegaT version ${OStrings.VERSION} (${OStrings.REVISION})")
